0c3dfce0180b0bfd94689ba5a7b4ab1530cc8ca6
Shalom Bhooshi <s.bhooshi@gmail.com>
2014-06-08 16:50:32 +0100

    Commit 1402242632

---
+++ lib/utils.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function patch_yaml {
+  local file="$1";  shift;
+  local key="$1";   shift;
+
+  cp -a "$file" "$file.$$.$(date +%s)"
+
+  perl -le '
+    use strict;
+    use warnings;
+    use autodie;
+    use YAML qw[Load DumpFile];
+
+    print join "|", 
+    my ($file, $key, @values) = (@ARGV);
+
+    my $yaml = do { 
+      local $/ = undef;
+      open my $fh, "<", $file; 
+      my $yaml = Load(<$fh>); 
+    };
+    my $ptr = \$yaml;
+    for my $iter (split m{/}, $key) { 
+      $ptr = \$$ptr->{$iter}; 
+    }
+    $$ptr = [ @values ];
+    eval { DumpFile($file, $yaml) };
+    die "$@" if $@;
+  ' "$file" "$key" "$@"
+
+}
+
