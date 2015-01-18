5392e0dbb47cc208044ddaa3b428d940775153be
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-01-18 23:13:36 +0000

    Set fileformat to unix to avoid double line spacing issues.

--- razor/task/winpe.task/imageprep/Modules/WindowsUpdate/WindowsUpdate.psm1
+++ razor/task/winpe.task/imageprep/Modules/WindowsUpdate/WindowsUpdate.psm1
@@ -8,45 +8,78 @@ $Global:UpdateSession = $Null # Scoped global: (not script:) to overcome piping 
 
 # TODO
 # # Configure policy settings, frequencies, reboot policies, etc
+# http://technet.microsoft.com/en-us/library/cc708449(v=ws.10).aspx
+# http://technet.microsoft.com/en-gb/library/dd939844(v=ws.10).aspx
 # http://smallvoid.com/article/winnt-automatic-updates-config.html
 
 function Enable-WindowsUpdate {
-  [CmdletBinding()] Param()
+  [CmdletBinding()] Param(
+    [ValidateSet('Manual','Automatic')]
+      [String]  $StartupType = 'Manual',
+    [Switch] $AutoInstallMinorUpdates = $False,
+    [Switch] $NoAutoRebootWithLoggedOnUsers = $True,
+    [Switch] $StartWindowsUpdateService = $True
+  )
   Write-Verbose "Enabling Windows Updates"
-  & reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"  /v AUOptions      /f | Write-Verbose
-  & reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"  /v NoAutoUpdate   /f | Write-Verbose
-  & reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"                 /v NoAutoUpdate   /f | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"                 /v AutoInstallMinorUpdates   /t REG_DWORD /d 0x1 /f | Write-Verbose
-  # & net start wuauserv
-  Set-Service wuauserv -StartupType manual -Verbose:$VerbosePreference
-  Start-Service wuauserv -Verbose:$VerbosePreference
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AutoInstallMinorUpdates       /t REG_DWORD /d ([Int][Boolean]$AutoInstallMinorUpdates) /f | Write-Verbose
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d ([Int][Boolean]$NoAutoRebootWithLoggedOnUsers) /f | Write-Verbose
+  if ($StartWindowsUpdateService) {
+    Get-Service wuauserv |
+      Set-Service -StartupType $StartupType -Verbose:$VerbosePreference -PassThru |
+      Restart-Service -Verbose:$VerbosePreference
+  }
+<#
+.SYNOPSIS
+Enable the windows update service and start it if required.
+
+.DESCRIPTION
+Enable the windows update service and start it if required.
+
+.PARAMETER StartupType
+Set the windows update service startuptype - can be one of Manual (default) or Automatic.
+
+.PARAMETER AutoInstallMinorUpdates
+Allow the windows update service to automatically install minor update that do not require reboots.
+
+.PARAMETER NoAutoRebootWithLoggedOnUsers
+Do not reboot the machine if there are currently logged on users.
+
+.PARAMETER StartWindowsUpdateService
+Default is $True. If set to false, this just ensure that the registry entries to allow windows update to be enabled are set.
+#>
 }
 
 function Disable-WindowsUpdate {
-  [CmdletBinding()] Param()
+  [CmdletBinding()] Param(
+    [ValidateSet('Manual','Disabled')]
+      [String]  $StartupType = 'Manual'
+  )
   Write-Verbose "Disabling Windows Updates"
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"  /v AUOptions      /t REG_DWORD /d 0x1 /f | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"  /v NoAutoUpdate   /t REG_DWORD /d 0x1 /f | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"                 /v NoAutoUpdate   /t REG_DWORD /d 0x1 /f | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"                 /v AutoInstallMinorUpdates   /t REG_DWORD /d 0x0 /f | Write-Verbose
-  # & net stop wuauserv
-  Set-Service wuauserv -StartupType disabled -Verbose:$VerbosePreference
-  Stop-Service wuauserv -Verbose:$VerbosePreference
+  Get-Service wuauserv | Set-Service -StartupType $StartupType -Verbose:$VerbosePreference -PassThru | Stop-Service -Verbose:$VerbosePreference
+<#
+.SYNOPSIS
+Set the windows update service startup type to be manual (not automatic).
+
+.DESCRIPTION
+Set the windows update service startup type to be manual (not automatic).
+
+.PARAMETER StartupType
+Set the windows update service startup type to be one of Manual (default) or Disabled.
+#>
 }
 
 function Get-WindowsUpdateSettings {
   [CmdletBinding()] Param()
   Write-Verbose "Getting Windows Updates settings"
-  # $Result = New-Object PSObject
   $Result = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" | Select -Exclude PS* *
 
   $WUAuServ = Get-Service wuauserv
-  $WUAuServRunning  = if ( $WUAuServ | ?{ $_.Status -eq 'Running' } ) { $True } else { $False } 
+  $WUAuServRunning  = if ( $WUAuServ | ?{ $_.Status -eq 'Running' } ) { $True } else { $False }
   $WUAuServStatus   = $WUAuServ | %{ $_.Status }
 
   $Result =
   $Result | Add-Member -PassThru NoteProperty ServiceRunning  $WUAuServRunning -Force |
-            Add-Member -PassThru NoteProperty ServiceStatus   $WUAuServStatus  -Force 
+            Add-Member -PassThru NoteProperty ServiceStatus   $WUAuServStatus  -Force
 
   $AuPolicy = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" | Select -Exclude PS* *
   $AuPolicy | gm -MemberType NoteProperty | %{
@@ -54,6 +87,11 @@ function Get-WindowsUpdateSettings {
   }
 
   $Result
+
+<#
+.SYNOPSIS
+List setting related to the windows update service.
+#>
 }
 
 function Set-WSUSServer {
@@ -66,15 +104,32 @@ function Set-WSUSServer {
       [Switch] $DoNotConnectToWindowsUpdateInternetLocations=$True
   )
 
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
       /f /v WUServer       /t REG_SZ     /d $WUServer        | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
       /f /v WUStatusServer /t REG_SZ     /d $WUStatusServer  | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" `
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" `
       /f /v UseWUServer    /t REG_DWORD  /d 0x1              | Write-Verbose
-  & reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
-      /f /v DoNotConnectToWindowsUpdateInternetLocations  /t REG_DWORD                    `
+  & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"    `
+      /f /v DoNotConnectToWindowsUpdateInternetLocations  /t REG_DWORD      `
       /d ([Int][Boolean]$DoNotConnectToWindowsUpdateInternetLocations) | Write-Verbose
+
+<#
+.SYNOPSIS
+Configure the WSUS client to point at a given WSUS URL.
+
+.DESCRIPTION
+Configure the WSUS client to point at a given WSUS URL.
+
+.PARAMETER WUServer
+URL of the WUServer (No validation is performed).
+
+.PARAMETER WUStatusServer
+URL of the WUStatusServer (No validation is performed).
+
+.PARAMETER DoNotConnectToWindowsUpdateInternetLocations
+Instruct the WSUS client to never connect to locations other than the WSUS server.
+#>
 }
 
 function Get-WindowsUpdateResults {
@@ -82,12 +137,12 @@ function Get-WindowsUpdateResults {
 
   if ( Test-Path ($BasePath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\Results") -ea 0 ) {
     $Result = New-Object PSObject
-    
+
     try {
       $Detect   = gp (Join-Path $BasePath 'Detect')   | Select -Exclude PS* *
       $Detect   | gm -MemberType NoteProperty | %{ $Result | Add-Member NoteProperty "Detect$($_.Name)"   $Detect.($_.Name)   }
     } catch {}
-  
+
     try {
       $Download = gp (Join-Path $BasePath 'Download') | Select -Exclude PS* *
       $Download | gm -MemberType NoteProperty | %{ $Result | Add-Member NoteProperty "Download$($_.Name)" $Download.($_.Name) }
@@ -122,7 +177,7 @@ function Assert-IsKB898461Installed {
 <#
 .SYNOPSIS
 Assert that hotfix 898461 is installed on the system. This hotfix is necessary
-for the new Windows Update mechanisms to function on Legacy Systems (Windows
+for the new Windows Update mechanisms to function on Legacy Systems (Windows XP/2003).
 
 .DESCRIPTION
 Software update 898461 installs the files for the Package Installer for Windows version 6.1.22.4.
@@ -266,8 +321,7 @@ function Search-WindowsUpdate {
   [CmdletBinding()] Param(
     [switch]$ImportantOnly,
     [switch]$All,
-    [switch]$History,
-    [switch]$DownloadOnly
+    [switch]$History
   )
 
   Write-Verbose $MyInvocation.MyCommand
@@ -296,104 +350,194 @@ function Search-WindowsUpdate {
   Write-Verbose "Number of updates found: $($SearchResult.Updates.Count)"
 
   return $SearchResult.Updates
+
+<#
+.SYNOPSIS
+Search for available windows updates.
+
+.DESCRIPTION
+Search for available windows updates.
+
+.PARAMETER ImportantOnly
+Search for and return important updates only.
+
+.PARAMETER All
+Search and return All windows updates.
+
+.PARAMETER History
+Search for and return the updates that were previously applied.
+
+.EXAMPLE
+$AvailableUpdates = Search-WindowsUpdate -ImportantOnly
+$AvailableUpdates | Install-WindowsUpdate
+#>
+
 }
 
 function Install-WindowsUpdate {
-[CmdletBinding()] Param(
-  [Switch]$DownloadOnly,
-  [Parameter(Position=0, Mandatory=$False, ValueFromPipeline=$True)] $SearchResult
-)
+  [CmdletBinding()] Param(
+    [Switch]$DownloadOnly,
+    [Parameter(Position=0, Mandatory=$False, ValueFromPipeline=$True)] $SearchResult
+  )
 
-begin {
-  Write-Verbose $MyInvocation.MyCommand
-  Write-Verbose "  -DownloadOnly  : $DownloadOnly"
-  $updatesToInstall  = New-Object -ComObject Microsoft.Update.UpdateColl
-  $Global:candidatesToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
-}
-
-process {
-  if ($SearchResult.InstallationBehavior.CanRequestUserInput) {
-    Write-Host -ForegroundColor Magenta "Update requires user input: " $SearchResult.Title
-    $SearchResult | fl *
-  }
-  else {
-    $IsInstalled = $SearchResult.IsInstalled
-    $IsDownloaded = $SearchResult.IsDownloaded
-    Write-Verbose   " Candidate: $($SearchResult.Title)"
-    Write-Verbose   "   IsInstalled: $IsInstalled"
-    Write-Verbose   "   IsDownloaded: $IsDownloaded"
-    if (-not($IsInstalled)) { $updatesToInstall.Add( $SearchResult ) > $Null }
-  }
-}
-
-end {
-  if ( -not($updatesToInstall.Count) ) {
-    Write-Verbose "$($updatesToInstall.Count) candidates to install. Exiting."
-    return
+  begin {
+    Write-Verbose $MyInvocation.MyCommand
+    Write-Verbose "  -DownloadOnly  : $DownloadOnly"
+    $updatesToInstall  = New-Object -ComObject Microsoft.Update.UpdateColl
+    $Global:candidatesToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
   }
 
-  $updatesToInstall | ?{ -not($_.IsDownloaded) } | % { $Global:candidatesToDownload.Add($_) > $Null}
-
-  if ($Global:candidatesToDownload.Count) {
-    $downloader         = $Global:UpdateSession.CreateUpdateDownloader()
-    $downloader.Updates = $Global:candidatesToDownload
-    try {
-      Write-Verbose "Downloading updates ..."
-      $downloader.Download() > $Null
-      Write-Verbose "Downloads complete."
+  process {
+    if ($SearchResult.InstallationBehavior.CanRequestUserInput) {
+      Write-Host -ForegroundColor Magenta "Update requires user input: " $SearchResult.Title
+      $SearchResult | fl *
     }
-    catch {
-      Write-Verbose "Problem downloading updates." $_
-      Write-Verbose " " $error[0]
+    else {
+      $IsInstalled = $SearchResult.IsInstalled
+      $IsDownloaded = $SearchResult.IsDownloaded
+      Write-Verbose   " Candidate: $($SearchResult.Title)"
+      Write-Verbose   "   IsInstalled: $IsInstalled"
+      Write-Verbose   "   IsDownloaded: $IsDownloaded"
+      if (-not($IsInstalled)) { $updatesToInstall.Add( $SearchResult ) > $Null }
     }
   }
 
-  if ($DownloadOnly) {
-    Write-Verbose "-DownloadOnly ($DownloadOnly) requested, returning .."
-    return $updatesToDownload
+  end {
+    if ( -not($updatesToInstall.Count) ) {
+      Write-Verbose "$($updatesToInstall.Count) candidates to install. Exiting."
+      return
+    }
+
+    $updatesToInstall | ?{ -not($_.IsDownloaded) } | % { $Global:candidatesToDownload.Add($_) > $Null}
+
+    if ($Global:candidatesToDownload.Count) {
+      $downloader         = $Global:UpdateSession.CreateUpdateDownloader()
+      $downloader.Updates = $Global:candidatesToDownload
+      try {
+        Write-Verbose "Downloading $($updatesToInstall.Count) updates ..."
+        $downloader.Download() > $Null
+        Write-Verbose "Downloads complete."
+      }
+      catch {
+        Write-Verbose "Problem downloading updates." $_
+        Write-Verbose " " $error[0]
+      }
+    }
+
+    if ($DownloadOnly) {
+      Write-Verbose "-DownloadOnly ($DownloadOnly) requested, returning .."
+      return $updatesToDownload
+    }
+
+
+    Write-Verbose "Candidates to install: $($updatesToInstall.Count)"
+    $i=0; $updatesToInstall | %{
+      Write-Verbose "  * $($i++) $($_.Title)";
+      $_.AcceptEula()
+    }
+
+    Write-Verbose "Setting update installer preferences for automation ..."
+    $installer                    = $Global:UpdateSession.CreateUpdateInstaller()
+    $installer.Updates            = $updatesToInstall
+    $installer.AllowSourcePrompts = $false
+    $installer.ForceQuiet         = $true
+
+    Write-Verbose "Installing $($updatesToInstall.Count) updates ..."
+    $installationResult = $installer.Install()
+
+    Write-Verbose "Installation Result: $($installationResult.Resultcode)"
+    Write-Verbose "Reboot Required: $($installationResult.RebootRequired)"
+
+    return $installationResult
   }
 
+<#
+.SYNOPSIS
+Install a collection of updates passed in via the pipeline.
 
-  Write-Verbose "Candidates to install: $($updatesToInstall.Count)"
-  $i=0; $updatesToInstall | %{
-    Write-Verbose "  * $($i++) $($_.Title)";
-    $_.AcceptEula()
+.DESCRIPTION
+Install a collection of updates (via Search-WindowsUpdate) passed in via the pipeline.
+These can also be passed in via the $SearchResult parameter.
+
+.PARAMETER SearchResult
+A collection of windows updates as returned via Search-WindowsUpdate
+
+.PARAMETER DownloadOnly
+Download but do not apply windows update
+#>
+
+}
+
+function Install-ImportantWindowsUpdates {
+  [CmdletBinding()] Param(
+    [Switch] $RebootIfNecessary
+  )
+
+  Write-Verbose "Installing important windows updates"
+  Enable-WindowsUpdate
+
+  $UpdateResults = @()
+  $Count = 0
+
+  while ($UpdateCollection = Search-WindowsUpdate -ImportantOnly -Verbose:$VerbosePreference) {
+    if ( $UpdateResult = $UpdateCollection | Install-WindowsUpdate -Verbose:$VerbosePreference ) {
+      $UpdateResults += $UpdateResult
+
+      $Result = "  Count: {0}, ResultCode: {1}, RebootRequired: {2}" `
+                  -f $Count, $UpdateResult.ResultCode, $UpdateResult.RebootRequired
+      Write-Verbose $Result
+
+      if ( (([Boolean]($UpdateResult.RebootRequired)) -and $RebootIfNecessary) -or ($Count -ge 5) ) {
+        Restart-Computer
+      }
+
+      $Count++
+    }
   }
 
-  Write-Verbose "Setting update installer preferences for automation ..."
-  $installer                    = $Global:UpdateSession.CreateUpdateInstaller()
-  $installer.Updates            = $updatesToInstall
-  $installer.AllowSourcePrompts = $false
-  $installer.ForceQuiet         = $true
+  Disable-WindowsUpdate
+  return $UpdateResults
 
-  Write-Verbose "Installing updates..."
-  $installationResult = $installer.Install()
+<#
+.SYNOPSIS
+Convenience function to search for and install important windows update.
 
-  Write-Verbose "Installation Result: $($installationResult.Resultcode)"
-  Write-Verbose "Reboot Required: $($installationResult.RebootRequired)"
+.DESCRIPTION
+Convenience function to search for and install important windows update.
+This function effectively calls
+Search-WindowsUpdate -ImportantOnly | Install-WindowsUpdate
 
-  return $installationResult
+.PARAMETER RebootIfNecessary
+Reboot the machine any updates require a reboot to be effectively applied.
+#>
+
 }
 
-}
 
 function Get-WindowsUpdateInstallerStatus {
   [CmdletBinding()] Param(
     [Switch] $IsBusy
   )
-  
+
   $MicrosoftUpdateInstaller = New-Object -ComObject "Microsoft.Update.Installer"
-  
+
   if ( $IsBusy ) {
     $MicrosoftUpdateInstaller.IsBusy
   }
   else {
     $MicrosoftUpdateInstaller
   }
+  <#
+  .SYNOPSIS
+  Return the status of the microsoft update installer
+
+  .PARAMETER IsBusy
+  Indicate whether or not the microsoft update installer is processing updates.
+  #>
 }
 
 function Get-WindowsUpdateSystemInfo {
-  [CmdletBinding()] Param (    
+  [CmdletBinding()] Param (
     [Switch] $RebootRequired
   )
   $SystemInfo = New-Object -ComObject "Microsoft.Update.SystemInfo"
@@ -407,14 +551,25 @@ function Get-WindowsUpdateSystemInfo {
 <#
 .SYNOPSIS
 Return the Microsoft.Update.SystemInfo COM Object Collection.
+
+.PARAMETER RebootRequired
+Indicate whether or not a reboot of the machine is required to complete update application.
 #>
+
 }
 
 function Get-WindowsUpdateHistory {
   [CmdletBinding()] Param()
   Search-WindowsUpdate -History
+
+<#
+.SYNOPSIS
+Convenience function/alias for Search-WindowsUpdate -History
+#>
+
 }
 
+# The following function is functionally incomplete and not tested.
 function Resolve-KBByID {
   Param(
     [Parameter(Mandatory=$True)] $ID
@@ -442,9 +597,9 @@ function Resolve-KBByID {
       return $False
     }
   }
-  
+
   $html = ((cat $targetFile) -join "")
-  
+
   $KbTitle = $MSBId = $KbDesc = $Revision = $DateCreated = $DateReviewed = $DateModified =
   $AppliesTo = $OSVersion = $Null
 
@@ -452,9 +607,9 @@ function Resolve-KBByID {
   [String] $KbTitle  = $KbTitle -replace '[\r\n\t]+',''
 
   [String]   $MSBId = ([Regex]::Match($KbTitle, '(MS\d+-\d+)\s*:')).Groups[1].Value
-  $MSBUrl = if ($MSBId) { 
+  $MSBUrl = if ($MSBId) {
     "http://technet.microsoft.com/en-gb/security/bulletin/$MSBID"
-  } else { $Null } 
+  } else { $Null }
 
   [String] $KbDesc   = ([Regex]::Match($html, '<meta name="Description" content="([^"]+)"')).Groups[1].Value
   [String] $KbDesc   = $KbDesc -replace '[\r\n\t]+',''
@@ -479,12 +634,12 @@ function Resolve-KBByID {
             Add-Member -PassThru NoteProperty Title       $KbTitle    |
             Add-Member -PassThru NoteProperty Description $KbDesc     |
             Add-Member -PassThru NoteProperty AppliesTo   $OSVersions |
-            Add-Member -PassThru NoteProperty LocalFile   $targetFile 
+            Add-Member -PassThru NoteProperty LocalFile   $targetFile
 
 <#
 .SYNOPSIS
 Get some basic information about a KB by ID using the support.microsoft.com
-webservice.
+webservice. WARNING: This function is not ready for production use.
 #>
 }
 
