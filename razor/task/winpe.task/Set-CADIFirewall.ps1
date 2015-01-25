8d496adf80ede573640b35045f686512e952fd33
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-01-25 00:30:15 +0000

    Fixup.

--- razor/task/winpe.task/Set-CADIFirewall.ps1
+++ razor/task/winpe.task/Set-CADIFirewall.ps1
@@ -1,18 +1,21 @@
 <#
 .SYNOPSIS
-   Script sets firewall for inbound CADI bootstrap.
+Script to set up firewall and services for CADI bootstrap.
 
-   Copyright (c) 2014 Automation, Citrix Systems UK Ltd.
+Copyright (c) 2014 Automation, Citrix Systems UK Ltd.
 
 .DESCRIPTION
-   Set inbound firewall rules to enable CADI to be installed
-   via PSEXEC call
+Set inbound firewall rules to
+  * Allow TCP ports 135 and 445 inbound (for PSExec)
+  * Allow ICMP Echo Request/Reply
+Start the following services
+  * Remote Registry
 #>
 
-$ports = @{}
-$ports.Add(135, "***AUTOMATION Open Port 135 For CADI bootstrap***")
-$ports.Add(445, "***AUTOMATION Open Port 445 For CADI bootstrap***")
-#
+$ports = @{
+  '135' = '***AUTOMATION Open Port 135 For CADI bootstrap***';
+  '445' = '***AUTOMATION Open Port 445 For CADI bootstrap***';
+}
 
 # If on winxp use  netsh firewall
 if([Environment]::OSVersion.Version.Major -lt 6) {
@@ -26,23 +29,30 @@ if([Environment]::OSVersion.Version.Major -lt 6) {
   netsh.exe advfirewall reset
 
   $ports.Keys | % {
-    Write-Host "Set Firewall Rules - Port=$_ Name=""$($ports[$_])""" 
+    Write-Host "Set Firewall Rules - Port=$_ Name='$($ports[$_])'" 
     netsh.exe advfirewall firewall add rule name=$($ports[$_]) `
        protocol=TCP localport=$_ dir=in action=allow profile=any enable=yes
     netsh.exe advfirewall firewall show rule name=$($ports[$_]) 
   }
 
-  Write-Host "Setting Firewall Rules - ICMPv4 Echo Request/Reply"
-  netsh.exe advfirewall firewall add rule name="Allow ICMPv4 Echo Request In" `
-      protocol=icmpv4:8,any dir=in  action=allow profile=any enable=yes
-  netsh.exe advfirewall firewall add rule name="Allow ICMPv4 Echo Reply Out" `
-      protocol=icmpv4:0,any dir=out action=allow profile=any enable=yes
+  Write-Host 'Setting Firewall Rules - ICMPv4 Echo Request/Reply'
+  netsh.exe advfirewall firewall add rule name='Allow ICMPv4 Echo Request In' `
+      protocol='icmpv4:8,any' dir=in  action=allow profile=any enable=yes
+  netsh.exe advfirewall firewall show rule name='Allow ICMPv4 Echo Request In'
 
-  Write-Host "Setting Firewall Rules - ICMPv6 Echo Request/Reply"
-  netsh.exe advfirewall firewall add rule name="Allow ICMPv6 Echo Request In" `
-      protocol=icmpv6:128,any dir=in  action=allow profile=any enable=yes
-  netsh.exe advfirewall firewall add rule name="Allow ICMPv6 Echo Reply Out" `
-      protocol=icmpv6:129,any dir=out action=allow profile=any enable=yes
+  netsh.exe advfirewall firewall add rule name='Allow ICMPv4 Echo Reply Out' `
+      protocol='icmpv4:0,any' dir=out action=allow profile=any enable=yes
+  netsh.exe advfirewall firewall show rule name='Allow ICMPv4 Echo Reply Out'
+
+  Write-Host 'Setting Firewall Rules - ICMPv6 Echo Request/Reply'
+  netsh.exe advfirewall firewall add rule name='Allow ICMPv6 Echo Request In' `
+      protocol='icmpv6:128,any' dir=in  action=allow profile=any enable=yes
+  netsh.exe advfirewall firewall show rule name='Allow ICMPv6 Echo Request In'
+
+  netsh.exe advfirewall firewall add rule name='Allow ICMPv6 Echo Reply Out' `
+      protocol='icmpv6:129,any' dir=out action=allow profile=any enable=yes
+  netsh.exe advfirewall firewall show rule name='Allow ICMPv6 Echo Reply Out'
+
 
   # Start Remote Registry
   if ( Get-Service RemoteRegistry ) {
