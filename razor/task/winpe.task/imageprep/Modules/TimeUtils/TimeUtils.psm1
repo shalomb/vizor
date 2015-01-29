204430d3cbb5c3e3611091d42af0eecd4e05138d
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-01-29 14:11:54 +0000

    Prune unneeded files.

--- razor/task/winpe.task/imageprep/Modules/TimeUtils/TimeUtils.psm1
+++
@@ -1,9 +0,0 @@
-# Script Module TimeUtils/TimeUtils.psm1
-
-Set-StrictMode -Version 2.0
-Set-PSDebug -Trace 0
-
-
-
-# vim:sw=2:ts=2:et:foldexpr=getline(v\:lnum)=~'^\s*$'&&getline(v\:lnum-1)=~'^\s*$'&&getline(v\:lnum+1)=~'\S'?'<1'\:1:fdm=expr:filetype=ps1:ff=dos:fenc=ASCII
-
