4331e0037d9753196fc5ee628af15aec3246a1eb
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-01-29 11:43:51 +0000

    Prune unneeded files.

--- razor/task/winpe.task/imageprep/StartupTasks/AsfShell/ASFShellBootstrap.cmd
+++
@@ -1,7 +0,0 @@
-@echo on
-
-setlocal enabledelayedexpansion
-
-set THISDIR=%~dp0
-
-powershell -nologo -noprofile -noexit -file "!THISDIR!\ASFShellBootstrap.ps1"
--- razor/task/winpe.task/imageprep/StartupTasks/AsfShell/ASFShellBootstrap.ps1
+++
@@ -1,9 +0,0 @@
-#AsfShell Bootstrap
-
-if (Get-Module -ListAvailable | ?{$_.Name -eq "Asf"}) {
-    Import-Module Asf
-} 
-else {
-  cd $Env:SystemDrive
-  iex((New-Object Net.WebClient).DownloadString('http://camautonfs01.eng.citrite.net/asf/irtt'))
-}
