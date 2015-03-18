3e245cf74b19b4a2afef3ecd322c2a9ba7c3952c
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-03-18 09:57:39 +0000

    Fixup OS Version check on PSv2.

--- razor/task/winpe.task/imageprep/Modules/TaskUtils/TaskUtils.psm1
+++ razor/task/winpe.task/imageprep/Modules/TaskUtils/TaskUtils.psm1
@@ -5,19 +5,17 @@ Set-PSDebug -Trace 0
 
 
 function Get-ScheduledTask {                    #M:TaskUtils
-  [CmdletBinding()]
-  Param()
+  [CmdletBinding()] Param()
   & schtasks.exe /query /v /fo csv | ConvertFrom-CSV | ?{ -not ( $_.TaskName -match "TaskName" ) }
 }
 
 
 function Disable-ScheduledTask {                #M:TaskUtils
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory=$True)] $taskName   
+  [CmdletBinding()] Param(
+    [Parameter(Mandatory=$True)] $taskName
   )
   Write-Verbose "Disabling Scheduled Task: $taskName" 
-  if ( (gwmi win32_operatingsystem).Version -lt 6 ) { # schtasks on XP & 2003 doesn't support /disable
+  if ( [Double](Get-OSVersion).CurrentVersion -lt 6 ) { # schtasks on XP & 2003 doesn't support /disable
     & schtasks /delete /tn "$taskName" /f       | Write-Verbose
   } else {
     & schtasks /change /tn "$taskName" /disable | Write-Verbose
