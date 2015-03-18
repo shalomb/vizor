fd6ffecdfe3dacfd90b37479c11d78054a8508d9
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-03-18 13:39:31 +0000

    Introduce Logging PS Module.

---
+++ razor/task/winpe.task/imageprep/Modules/Logging/Logging.psm1
@@ -0,0 +1,77 @@
+﻿Set-StrictMode -Version 2.0
+
+
+$DebugPreference = "Continue"
+
+function Write-MessageToStream {
+  <#
+.NOTES
+  Care to be taken, Write-MessageToStream does not honour the
+  preference variables (under variable:\*Preference*)
+  and introduce output on streams that the user does not necessarily want.
+.TODO
+  Reconsider as Bug??
+  #>
+  [CmdletBinding()]
+  Param(
+    [Alias("OutputMessage")]  [PSObject]        $Message,
+    [ValidateSet('host','debug','error','progress','verbose','warning')] [Alias("OutputStream")]
+      $Stream = "host"
+  )
+  if ($Stream -ne $Null) {
+    Write-Verbose "Writing '$Message' on '$Stream'"
+    switch ( $Stream.ToLower() ) {
+      'host'      { Write-Host      $Message }
+      'debug'     { Write-Debug     $Message }
+      'error'     { $Host.UI.WriteErrorLine( $Message); [Console]::Error.Flush(); }
+      'progress'  { Write-Progress  $Message }
+      'verbose'   { Write-Verbose   $Message }
+      'warning'   { Write-Warning   $Message }
+      default     { throw "Invalid stream: '$Stream'" }
+    }
+  }
+  else {
+    $Host.UI.WriteLine( $Message )
+  }
+}
+
+function Trace-CallStack {
+  [CmdletBinding()]
+  Param(
+    [Alias("Message")]          [PSObject]      $TraceMessage,
+    [Alias("OutputStream")]     [System.String] $Stream   = "Error",
+    [Alias("Property")]         [System.Array]  $PSCallStackProperties = @("Location", "Command", "Arguments"),
+    [Alias("FramesToSkip")]     [System.Int32]  $SkipFrames     = 1,
+    [Alias("NoVerbose")]        [Switch]        $Terse    = $False,
+    [Alias("UseDecoratations")] [Switch]        $Decorate = $False
+  )
+
+  $PSCallStack         = (Get-PSCallStack)
+  $CallerFrame         = ($PSCallStack)[1]
+  $TraceBackLeaderMsg  = "TraceBack (most recent call last): "
+
+  if ($TraceMessage -eq $Null) { $TraceMessage = $CallerFrame.InvocationInfo.Line -replace "`n|^\s+", "" }
+
+  if ($Decorate) { Write-MessageToStream -Stream $Stream -Message ("-" * 40) }
+  if ($Terse) {
+    $TraceMessage = ($PSCallStackProperties | %{ $CallerFrame.($_) }) -join " : "
+    Write-MessageToStream -Stream $Stream -Message "$TraceBackLeaderMsg $TraceMessage"
+    $SkipFrames++
+  }
+  else {
+    Write-MessageToStream -Stream $Stream -Message "$TraceBackLeaderMsg $TraceMessage"
+
+    $PSCallStack |
+      Select-Object -Skip $SkipFrames |
+      %{  $frame = $_
+          Write-MessageToStream -Stream $Stream `
+            -Message ("  " + ( ($PSCallStackProperties | %{ $frame.($_) }) -join " : "))
+      }
+
+    if ($Stream -ieq 'error') { Write-Error (($PSCallStackProperties | %{ $PSCallStack[0].($_) }) -join " : ") }
+  }
+  if ($Decorate) { Write-MessageToStream -Stream $Stream -Message ("-" * 40) }
+}
+
+Export-ModuleMember Show-*,Write-*,Trace-*,Throw*
+
---
+++ razor/task/winpe.task/imageprep/Modules/Logging/tests/Test-Logging.ps1
@@ -0,0 +1,60 @@
+﻿Set-StrictMode -Version 2.0
+
+$ErrorActionPreference="Stop"
+$VerbosePreference="Continue"
+$DebugPreference="WhatContinue"
+$Global:TraceCallStackPreference="FOOBIEs"
+# $Global:VerbosePreference="SilentlyContinue"
+# $Global:VerbosePreference="Continue"
+
+  $Env:PSModulePath+=";$($PWD.ProviderPath)"
+  Write-Verbose "`$Env:PSModulePath=$Env:PSModulePath"
+  Write-Verbose "Import-Module PowerShell/Logging"
+Import-Module PowerShell/Logging -Force
+
+function baz {
+  [CmdletBinding()]
+  Param($p)
+  Trace-CallStack
+}
+
+function bar {
+  [CmdletBinding()]
+  Param($p)
+  Trace-CallStack
+  baz($p)
+}
+
+function foo {
+  [CmdletBinding()]
+  Param($p, [Switch]$UseDecorations)
+  # Trace-CallStack -Terse
+  bar($p)
+}
+
+Write-Host      "Baz"
+Trace-CallStack "Baz"
+baz
+Write-Host      "Bar"
+Trace-CallStack "Bar"
+bar
+Write-Host      "Foo"
+Trace-CallStack "Foo"
+foo "one"
+exit
+
+# Test Case - Simple Call
+$Tests = @{
+  "Simple Function Call" = @{ Test = { baz "Hello World!!" }; Result = ""; };
+  "Nested Function Call" = @{ Test = { foo "Hello World!!" }; Result = ""; };
+}
+# $Tests | fl *
+
+$Tests.Keys | %{
+  $Tests[$_]["Result"] = ($Tests[$_]["Test"]).Invoke()
+}
+
+$Tests.Keys | %{
+  $msg = "{0}  =>  {1}" -f $_, $Tests[$_]["Result"]
+  Write-Host $msg
+}
