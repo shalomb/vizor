9d01fff33c951f246a7b8eaf5b9def036b0df3b3
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-05-15 18:35:03 +0100

    Upgrade wimlib from wimlib-1.7.2 to wimlib-1.7.4

--- lib/razor-winpe.sh
+++ lib/razor-winpe.sh
@@ -7,7 +7,7 @@ set -a
          ipxe_url='git://git.ipxe.org/ipxe.git'
 kb3aik_en_iso_url='http://download.microsoft.com/download/8/E/9/8E9BBC64-E6F8-457C-9B8D-F6C9A16E6D6A/KB3AIK_EN.iso'
       wimboot_url='https://git.ipxe.org/releases/wimboot/wimboot-2.3.0.tar.bz2'
-       wimlib_url='http://sourceforge.net/projects/wimlib/files/wimlib-1.7.2.tar.gz'
+       wimlib_url='http://sourceforge.net/projects/wimlib/files/wimlib-1.7.4.tar.gz'
 
 overlay_dir='/usr/src/winpe-overlay'
 razor_client_script="$PWD/razor-client.cmd"
