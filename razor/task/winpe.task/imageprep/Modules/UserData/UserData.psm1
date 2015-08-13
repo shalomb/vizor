12c27cb0cd8b918303f8bf62a3a4e961cac6760d
Shalom Bhooshi <s.bhooshi@gmail.com>
2015-08-13 14:49:08 +0100

    Add in support for XenStore bootstrap (base64 payload).

---
+++ razor/task/winpe.task/imageprep/Modules/UserData/UserData.psm1
@@ -0,0 +1,39 @@
+﻿# PowerShell
+
+Set-StrictMode -Version 2.0
+
+function ConvertFrom-Base64 {
+  [CmdletBinding()] Param(
+    [String] $Base64
+  )
+  [System.Text.Encoding]::UTF8.GetString( [System.Convert]::FromBase64String($Base64) )
+}
+
+function ConvertTo-Base64 {
+  [CmdletBinding()] Param(
+    [String] $String
+  )
+  [System.Convert]::ToBase64String( [System.Text.Encoding]::UTF8.GetBytes($String) )
+}
+
+function Invoke-UserData {
+  [CmdletBinding()] Param(
+    [String] $Base64,
+    [String] $String
+  )
+
+  if ( $Base64 ) {
+    $String = ConvertFrom-Base64 $Base64
+  }
+
+  if ( $String ) {
+    Switch ( $String.GetType() ) {
+      'String'      { iex $String      }
+      'ScriptBlock' { $String.Invoke() }
+    }
+  }
+  else {
+    Throw "No UserData supplied."
+  }
+}
+
